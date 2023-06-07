087388c2f62bbed7c08bb176140ccac949514e83
unknown <etbcor@gmail.com>
2023-06-07 10:30:35 -0700

    added method to read edges from a file

--- proj3/src/main.rs
+++ proj3/src/main.rs
@@ -1,9 +1,47 @@
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
 use graph::prelude::*;
 
 fn main() {
-    let g: DirectedCsrGraph<usize> = GraphBuilder::new()
+    let graph: UndirectedCsrGraph<usize> = GraphBuilder::new()
         .csr_layout(CsrLayout::Sorted)
-        .edges(vec![(0, 1), (0, 2), (1, 2), (1, 3), (2, 3)])
+        .edges(edges_from_file("CSPData.csv"))
         .build();
-    println!("asdfa");
+
+    println!(
+        "node count: {}, edge count: {}",
+        graph.node_count(),
+        graph.edge_count()
+    );
+}
+
+fn edges_from_file(path: &str) -> Vec<(usize, usize)> {
+    let file = File::open(path).expect("couldn't open file");
+    let mut reader = BufReader::new(file);
+    let mut labels = parse_line(&mut reader);
+    labels.remove(0);
+    // println!("    {:?}", labels);
+    let num_vars = labels.len();
+    assert!(num_vars > 1, "not enough variables");
+    let mut edges = vec![];
+    for row in 0..num_vars - 1 {
+        let line = parse_line(&mut reader);
+        // println!("{:?}", line);
+        assert_eq!(line.len() - 1, num_vars, "line is the wrong size");
+        for (col, string) in line.iter().skip(1).enumerate() {
+            if string == "1" {
+                edges.push((row, col));
+            }
+        }
+    }
+    edges
+}
+
+fn parse_line(reader: &mut BufReader<File>) -> Vec<String> {
+    let mut line = String::new();
+    reader.read_line(&mut line).expect("couldn't read line");
+    line.trim().split(',').map(|x| x.to_string()).collect()
 }
